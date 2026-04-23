131e255d79709677de5e7f8279a0ac3302abb81a
Abul Bashar <bashar35790@gmail.com>
2026-04-23 11:58:15 +0600

    setup heroUi

---
+++ app/auth/signUp/page.tsx
@@ -0,0 +1,10 @@
+import {Check} from "@gravity-ui/icons";
+import {Button, Description, FieldError, Form, Input, Label, TextField} from "@heroui/react";
+
+function SignUp() {
+  return (
+    <div>SignUp</div>
+  )
+}
+
+export default SignUp;
